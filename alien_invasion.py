fedae1f8f58e878e42f9758715e8d927f8e6a504
MGoraya777 <154380083+MGoraya777@users.noreply.github.com>
2025-07-27 22:35:58 -0400

    Lab 11 Video 1

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,27 @@
 import sys
 import pygame
 
+class AlienInvasion:
+
+    def __init__(self_) -> None:
+        pygame.init()
+
+        self_.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption('Alien Invasion')
+
+        self_.running = True
+    
+    def run_game(self):
+        # Game loop
+        while self.running:
+            for event in pygame.event.get(): 
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            pygame.display.flip()
+
 if __name__ == "__main__":
-    """Test comment"""
+    ai = AlienInvasion()
+    ai.run_game()
     pass
