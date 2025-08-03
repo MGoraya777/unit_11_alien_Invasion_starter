5f49893ab8215703dc2e9d27bce7bc790d911ad8
MGoraya777 <154380083+MGoraya777@users.noreply.github.com>
2025-08-03 15:43:14 -0400

    Lab 11 Video 2

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,15 +1,23 @@
 import sys
 import pygame
-
+from settings import Settings
 class AlienInvasion:
 
     def __init__(self_) -> None:
         pygame.init()
+        self_.settings = Settings()
 
-        self_.screen = pygame.display.set_mode((1200,800))
+        self_.screen = pygame.display.set_mode(
+            (self_.settings.screen_w, self_.settings.screen_h))
         pygame.display.set_caption('Alien Invasion')
 
+        self_.bg = pygame.image.load(self_.settings.bg_file)
+        self_.bg = pygame.transform.scale(
+            self_.bg,
+            (self_.settings.screen_w, self_.settings.screen_h))
+
         self_.running = True
+        self_.clock = pygame.time.Clock()
     
     def run_game(self):
         # Game loop
@@ -18,8 +26,9 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-
+            self.screen.blit(self.bg, (0,0))
             pygame.display.flip()
+            self.clock.tick(self.settings.FPS)
 
 if __name__ == "__main__":
     ai = AlienInvasion()
